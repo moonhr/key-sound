0e9bf1d2e0e1d865661604dc5eeaf68329497010
moonhr <moonhr153@gmail.com>
2024-08-25 22:36:36 +0900

    Test | 사운드 테스트

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+import SoundSelector from "@/components/sound_selector";
+import { API_ENDPOINTS } from "@/ts/end_point";
+
 export default function Home() {
   return (
     <>
       <div>사운드 선택</div>
+      <SoundSelector
+        soundName="office-keyboard"
+        soundApiEndpoint={API_ENDPOINTS.SOUND}
+      />
+      <SoundSelector
+        soundName="Boom Short"
+        soundApiEndpoint={API_ENDPOINTS.SOUND}
+      />
+      <SoundSelector
+        soundName="Cartoon Walking Sound"
+        soundApiEndpoint={API_ENDPOINTS.SOUND}
+      />  
+      <SoundSelector
+        soundName="iphone"
+        soundApiEndpoint={API_ENDPOINTS.SOUND}
+      />
     </>
   );
 }
