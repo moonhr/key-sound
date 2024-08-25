a433f4f8d4668d084a88e5e71d13b6a196521daf
moonhr <moonhr153@gmail.com>
2024-08-25 19:11:16 +0900

    Fix | 서버 cors 로컬호스트 3000 허용

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -7,7 +7,21 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // 미들웨어 설정
-app.use(cors()); // CORS 설정 (필요한 경우 도메인 제한 가능)
+
+// CORS 설정
+const allowedOrigins = ["http://localhost:3000"]; // 허용할 도메인 목록
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin as string) !== -1 || !origin) {
+        callback(null, true); // 허용된 도메인
+      } else {
+        callback(new Error("Not allowed by CORS")); // 허용되지 않은 도메인
+      }
+    },
+  })
+);
 app.use(morgan("dev")); // 로그 기록 (개발 환경)
 app.use(express.json()); // JSON 요청 바디 파싱
 app.use(express.static(path.join(__dirname, "../frontend/public"))); // 정적 파일 제공
