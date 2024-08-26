24991848961e3066f516c767c227823749f08769
moonhr <moonhr153@gmail.com>
2024-08-26 11:09:50 +0900

    Fix | coutom hook, sound_selector, textarea를 하나의 컴포넌트로 모아 사용함.

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,27 +1,31 @@
+"use client";
 import React from "react";
 import SoundSelector from "@/components/sound_selector";
+import Textarea from "@/components/textarea";
 import { API_ENDPOINTS } from "@/ts/end_point";
+import { useSoundSelector } from "@/hooks/useSoundSelector";
 
 export default function Home() {
+  const { selectedSound, handleSelectSound, handlePlaySound } =
+    useSoundSelector({
+      soundApiEndpoint: API_ENDPOINTS.SOUND,
+    });
+
   return (
     <>
       <div>사운드 선택</div>
       <SoundSelector
         soundName="office-keyboard"
-        soundApiEndpoint={API_ENDPOINTS.SOUND}
+        onSelectSound={handleSelectSound}
       />
-      <SoundSelector
-        soundName="Boom Short"
-        soundApiEndpoint={API_ENDPOINTS.SOUND}
-      />
+      <SoundSelector soundName="Boom Short" onSelectSound={handleSelectSound} />
       <SoundSelector
         soundName="Cartoon Walking Sound"
-        soundApiEndpoint={API_ENDPOINTS.SOUND}
-      />  
-      <SoundSelector
-        soundName="iphone"
-        soundApiEndpoint={API_ENDPOINTS.SOUND}
+        onSelectSound={handleSelectSound}
       />
+      <SoundSelector soundName="iphone" onSelectSound={handleSelectSound} />
+
+      <Textarea soundName={selectedSound} onPlaySound={handlePlaySound} />
     </>
   );
 }
