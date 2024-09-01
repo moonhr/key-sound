95289b26dc9750df29101c3ec43c66fee1017fb3
moonhr <moonhr153@gmail.com>
2024-09-01 20:43:03 +0900

    Art | style

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -3,16 +3,18 @@
 
 import React, { useEffect } from "react";
 import SoundPlayer from "./components/sound_player";
+import { Keyboard } from "./components/keyboard";
 
 export default function Home() {
   // 페이지 새로고침 시 로컬 스토리지를 비우는 useEffect 훅
-  useEffect(() => {
-    localStorage.clear();
-  }, []);
+
 
   return (
-    <div>
-      <SoundPlayer />
-    </div>
+    <>
+      <div className="w-screen h-screen">
+        <SoundPlayer className="flex justify-center items-center h-full" />
+      </div>
+      {/* <Keyboard /> */}
+    </>
   );
 }
