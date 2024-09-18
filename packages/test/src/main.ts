e9387883c7a142ea746d8195fcbe50d1ed7575bc
moonhr <moonhr153@gmail.com>
2024-09-18 21:27:15 +0900

    globalShortcut 를 이용한 키 감지

--- packages/test/src/main.ts
+++ packages/test/src/main.ts
@@ -1,4 +1,11 @@
-import { app, BrowserWindow, Tray, nativeImage, session } from "electron";
+import {
+  app,
+  BrowserWindow,
+  Tray,
+  nativeImage,
+  session,
+  globalShortcut,
+} from "electron";
 // import { TrayMenu } from "./electron/TrayMenu";
 import * as path from "path";
 import * as dotenv from "dotenv";
@@ -94,6 +101,31 @@ app.whenReady().then(() => {
   tray.setToolTip("Next.js Menubar App");
 
   tray.on("click", toggleMenuWindow);
+
+  // 알파벳과 숫자 배열 생성
+  const keys = [
+    ..."abcdefghijklmnopqrstuvwxyz".split(""), // a-z 알파벳
+    ..."0123456789".split(""), // 0-9 숫자
+  ];
+
+  // 각 키에 대해 globalShortcut 등록
+  keys.forEach((key) => {
+    globalShortcut.register(key, () => {
+      console.log(`Global key pressed: ${key}`);
+      menuWindow.webContents.send("key-pressed", key); // 렌더러 프로세스로 키 이벤트 전송
+    });
+  });
+
+  // globalShortcut 설정
+  // globalShortcut.register("CommandOrControl+K", () => {
+  //   if (menuWindow) {
+  //     menuWindow.webContents.send("key-pressed", "K");
+  //   }
+  // });
+});
+
+app.on("will-quit", () => {
+  globalShortcut.unregisterAll(); // 앱 종료 시 전역 단축키 해제
 });
 
 app.on("window-all-closed", () => {
