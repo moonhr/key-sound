97d278077f1f60fceee6c92b3b276a75b859150d
moonhr <moonhr153@gmail.com>
2024-09-22 19:46:01 +0900

    Fix | 경로명 변경

--- packages/test/src/static/staticData.ts
+++ packages/test/src/static/staticData.ts
@@ -1,7 +1,11 @@
 import { SoundButton } from "./sound_button_interface";
-import BrownKey from "../static/svg/BrownKey.svg";
-import BrownKeyActive from "../static/svg/BrownKey_active.svg";
-import BrownKeySound from "../static/sound/brown-key.mp3";
+import BrownKeySound from "./sound/brown-key.mp3";
+import BrownKey from "./svg/BrownKey.svg";
+import BrownKeyActive from "./svg/BrownKey_active.svg";
+
+// const BrownKeySound = require("./sound/brown-key.mp3") as string;
+// const BrownKey = require("./svg/BrownKey.svg") as string;
+// const BrownKeyActive = require("./svg/BrownKey_active.svg") as string;
 
 export const staticData: SoundButton[] = [
   {
