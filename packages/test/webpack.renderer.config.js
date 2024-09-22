d03d572c8aff91dd8a52048a20133c1a3fdba4ae
moonhr <moonhr153@gmail.com>
2024-09-22 19:43:21 +0900

    renderer config

--- packages/test/webpack.renderer.config.js
+++ packages/test/webpack.renderer.config.js
@@ -1,72 +1,71 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
+// const webpack = require("webpack");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/public/renderer.tsx",
-  target: "web",
-  resolve: {
-    fallback: {
-      fs: false, // 브라우저에서는 fs 모듈이 필요 없으므로 false 처리
-      path: require.resolve("path-browserify"), // path 모듈을 path-browserify로 대체
-    },
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-    extensions: [".tsx", ".ts", ".js"],
-  },
+  target: "electron-renderer",
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
+        include: /src/,
+        use: "ts-loader",
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
         exclude: /node_modules/,
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
-            },
-          },
-        ],
       },
       {
         test: /\.(mp3|wav)$/, // mp3와 wav 파일을 처리
         type: "asset/resource",
+        // use: ["url-loader", "file-loader"],
         generator: {
-          filename: "static/sound/[name][ext]", // 파일명을 해싱 없이 유지
+          filename: "src/static/sound/[name][ext]", // 파일명을 해싱 없이 유지
         },
       },
       {
         test: /\.css$/,
+        type: "asset/resource",
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.svg$/, // svg 파일 처리
+        test: /\.svg$/,
         oneOf: [
           {
             issuer: /\.[jt]sx?$/, // JS, TSX 파일에서 사용하는 경우
-            use: ["@svgr/webpack", "url-loader"],
+            use: ["@svgr/webpack"],
+            type: "asset/resource",
+            generator: {
+              filename: "src/static/svg/[name][ext]", // 파일명을 해싱 없이 유지
+            },
           },
           {
             type: "asset/resource", // 일반적으로 파일로 사용되는 경우
-            use: "file-loader",
+            generator: {
+              filename: "src/static/svg/[name][ext]", // SVG 파일명을 해싱 없이 유지
+            },
           },
         ],
+        // type: "asset/resource",
+        // use: ["@svgr/webpack", "url-loader"],
       },
     ],
   },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
   output: {
-    path: path.resolve(__dirname, "dist"),
     filename: "renderer.js",
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/public/index.html",
+      inject: "body",
     }),
     new CopyPlugin({
       patterns: [{ from: "src/assets", to: "assets" }],
