515fb69afd1db54cd7e7c5e4aee60f49d8666b53
moonhr <moonhr153@gmail.com>
2024-09-27 12:59:12 +0900

    Feat | 파일 불러올 input창

--- packages/key-sound/src/renderer/components/top/load/load.tsx
+++ packages/key-sound/src/renderer/components/top/load/load.tsx
@@ -1,6 +1,27 @@
 import React from "react";
+import { useSoundFile } from "../../../contexts/soune_file_context";
 
-const Load = () => {
-  return <div className="w-[25px] bg-yellow-200">로드</div>;
+const LoadFile: React.FC = () => {
+  const { setSoundFile } = useSoundFile();
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setSoundFile(file); // mp3 또는 mov 파일만 허용
+    } else {
+      alert("mp3 또는 mov 파일만 불러올 수 있습니다.");
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".mp3" // mp3 파일만 허용
+        onChange={handleFileChange}
+      />
+    </div>
+  );
 };
-export default Load;
+
+export default LoadFile;
