13beb1075836f9affcacd9d8bd77c51ff7754401
moonhr <moonhr153@gmail.com>
2024-09-27 12:58:18 +0900

    Feat |  파일명 띄워질 bar

--- packages/key-sound/src/renderer/components/top/bar/bar.tsx
+++ packages/key-sound/src/renderer/components/top/bar/bar.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useSoundFile } from "../../../contexts/soune_file_context";
 
-const Bar = () => {
-  return <div className="w-[320px] bg-red-600">bar</div>;
+const Bar: React.FC = () => {
+  const { soundFile } = useSoundFile();
+
+  return (
+    <div className="w-[200px] bg-slate-400">
+      {soundFile ? (
+        <p>불러온 파일명: {soundFile.name}</p>
+      ) : (
+        <p>파일이 불러오지 않았습니다.</p>
+      )}
+    </div>
+  );
 };
+
 export default Bar;
