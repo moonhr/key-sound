1dbc226c0cbd223387815190f93c0269860f27f3
moonhr <moonhr153@gmail.com>
2024-09-27 12:54:51 +0900

    Feat |  소리 재생 버튼 제작
    사운드 파일의 url을 재 정의
    url을 audioRef로 재 정의하여 담아둔다

--- packages/key-sound/src/renderer/components/top/play_button/play_buton.tsx
+++ packages/key-sound/src/renderer/components/top/play_button/play_buton.tsx
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { useSoundFile } from "../../../contexts/soune_file_context";
 
-const Play_button = () => {
-  return <div className="w-[25px] bg-lime-200">재생</div>;
+const PlayButton: React.FC = () => {
+  const { soundFile } = useSoundFile();
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+
+  useEffect(() => {
+    let fileUrl: string | null = null;
+
+    if (soundFile) {
+      fileUrl = URL.createObjectURL(soundFile);
+      if (audioRef.current) {
+        audioRef.current.src = fileUrl;
+      }
+    }
+
+    return () => {
+      if (fileUrl) {
+        URL.revokeObjectURL(fileUrl);
+      }
+    };
+  }, [soundFile]);
+
+  const handlePlay = () => {
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  return (
+    <div>
+      {/* 올바른 MIME 타입은 audio/mpeg */}
+      {soundFile?.type === "audio/mpeg" ? (
+        <audio ref={audioRef} controls />
+      ) : (
+        <p>MP3 파일만 재생할 수 있습니다.</p>
+      )}
+      <button onClick={handlePlay}>재생</button>
+    </div>
+  );
 };
-export default Play_button;
+
+export default PlayButton;
