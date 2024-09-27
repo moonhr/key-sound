47706f523359a607ad0c6fe961e6accdc16d8a49
moonhr <moonhr153@gmail.com>
2024-09-27 12:53:42 +0900

    Fix | sound provider적용

--- packages/key-sound/src/renderer/app/app.tsx
+++ packages/key-sound/src/renderer/app/app.tsx
@@ -3,11 +3,14 @@ import React from "react";
 
 import Top from "../components/top/top";
 import Main from "../components/main/main";
+import { SoundFileProvider } from "../contexts/soune_file_context";
 
 const AppContent: React.FC = () => {
   return (
     <div>
-      <Top />
+      <SoundFileProvider>
+        <Top />
+      </SoundFileProvider>
       <Main />
       {/* <TestComponent /> */}
     </div>
