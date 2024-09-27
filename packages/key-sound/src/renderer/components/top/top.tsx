b1a7f1ba8d48509c645bd2a3caa6aa12baa4be28
moonhr <moonhr153@gmail.com>
2024-09-27 12:58:33 +0900

    Fix | bar 추가

--- packages/key-sound/src/renderer/components/top/top.tsx
+++ packages/key-sound/src/renderer/components/top/top.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Load from "./load/load";
 import Play_button from "./play_button/play_buton";
+import Bar from "./bar/bar";
 import Key_sound from "./key_sound/key_sound";
 import Off_button from "./off_button/off_button";
+import { useSoundFile } from "../../contexts/soune_file_context";
 
 const Top = () => {
+  const { soundFile, setSoundFile } = useSoundFile();
   return (
     <div className="flex flex-row">
       <Load />
       <Play_button />
+      <Bar />
       <Key_sound />
       <Off_button />
     </div>
