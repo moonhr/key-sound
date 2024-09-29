96be8f48138c319a39680430ba3da552511789a3
moonhr <moonhr153@gmail.com>
2024-09-30 00:19:48 +0900

    모달창 생성

---
+++ packages/key-sound/src/renderer/components/modal/modal.tsx
@@ -0,0 +1,22 @@
+// Modal.tsx
+import React from "react";
+
+interface ModalProps {
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>
+          &times;
+        </span>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
