35be1e77954ae10f075ddf6477753bb1137ca532
moonhr <moonhr153@gmail.com>
2024-09-30 00:20:13 +0900

    모달 open

--- packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
+++ packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
@@ -1,6 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../../modal/modal";
+import TestComponent from "../../modal/test_component";
 
-const Key_sound = () => {
-  return <div className="w-[17px] bg-red-400">키</div>;
+const KeySound = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [keySound, setKeySound] = useState<string | null>(null);
+
+  const handleSaveKeySound = (selectedSound: string) => {
+    setKeySound(selectedSound);
+    setModalOpen(false); // 모달 닫기
+  };
+
+  return (
+    <div>
+      <button onClick={() => setModalOpen(true)}>모달 열기</button>
+      {isModalOpen && (
+        <Modal onClose={() => setModalOpen(false)}>
+          <TestComponent onSaveKeySound={handleSaveKeySound} />
+        </Modal>
+      )}
+      <div>
+        {keySound ? (
+          <p>Selected Sound: {keySound}</p>
+        ) : (
+          <p>키 사운드를 선택하세요!</p>
+        )}
+      </div>
+    </div>
+  );
 };
-export default Key_sound;
+
+export default KeySound;
