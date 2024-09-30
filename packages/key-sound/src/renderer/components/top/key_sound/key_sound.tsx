bc593a961131cab305bda01a586810a318cf36c9
moonhr <moonhr153@gmail.com>
2024-09-30 11:16:18 +0900

    Fix | 자식컴포넌트로부터 props전달받아 swtich

--- packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
+++ packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
 import Modal from "../../modal/modal";
-import TestComponent from "../../modal/test_component";
+import KeycapStatic from "../../modal/keycap_static";
+import { useKeycap } from "../../../contexts/keycap_context";
+import { staticData } from "../../../../static/staticData";
 
 const KeySound = () => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const [keySound, setKeySound] = useState<string | null>(null);
+  // const [keySound, setKeySound] = useState<string | null>(null);
+  const [selectedKeycap, setSelectedKeycap] = useState<{
+    svg: string;
+  } | null>(null);
+  const { selectedSound, setSelectedSound } = useKeycap();
 
-  const handleSaveKeySound = (selectedSound: string) => {
-    setKeySound(selectedSound);
+  const handleSaveKeySound = (selectedKey: string) => {
+    const selectedData = staticData[selectedKey];
+    setSelectedKeycap({
+      svg: selectedData.svg,
+    });
+    setSelectedSound(selectedData.soundFile);
+    console.log(selectedData.soundFile);
     setModalOpen(false); // 모달 닫기
   };
 
   return (
     <div>
       <button onClick={() => setModalOpen(true)}>모달 열기</button>
+      {/* 모달 창 */}
       {isModalOpen && (
         <Modal onClose={() => setModalOpen(false)}>
-          <TestComponent onSaveKeySound={handleSaveKeySound} />
+          <KeycapStatic onSaveKeySound={handleSaveKeySound} />
         </Modal>
       )}
+
+      {/* 선택된 키캡과 사운드가 있는지 확인 */}
       <div>
-        {keySound ? (
-          <p>Selected Sound: {keySound}</p>
+        {selectedKeycap ? (
+          <div>
+            <img src={selectedKeycap.svg} alt="Selected Keycap" />
+          </div>
         ) : (
           <p>키 사운드를 선택하세요!</p>
         )}
