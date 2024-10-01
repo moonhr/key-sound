8c9af5efbbe3f0705d1e2a7902357d2b7be974f8
moonhr <moonhr153@gmail.com>
2024-10-01 20:34:54 +0900

    기존 playbar가 piano안으로 이동됨

--- packages/key-sound/src/renderer/components/main/main.tsx
+++ packages/key-sound/src/renderer/components/main/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Piano from "./piano/piano";
-import Playbar from "./playbar/playbar";
 
 const Main = () => {
   return (
     <div>
       <Piano />
-      <Playbar />
     </div>
   );
 };
