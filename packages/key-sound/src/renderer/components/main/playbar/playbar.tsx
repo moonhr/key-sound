752339cf9357f365f5ce133efe9cc14a3d04978d
moonhr <moonhr153@gmail.com>
2024-10-01 20:06:54 +0900

    녹음 시작, 중지 playber

--- packages/key-sound/src/renderer/components/main/playbar/playbar.tsx
+++ packages/key-sound/src/renderer/components/main/playbar/playbar.tsx
@@ -1,6 +1,18 @@
+// Playbar.tsx
 import React from "react";
 
-const Playbar = () => {
-  return <div className="w-[250px] bg-yellow-500">Playbar</div>;
+interface PlaybarProps {
+  startRecording: () => void;
+  stopRecording: () => void;
+}
+
+const Playbar: React.FC<PlaybarProps> = ({ startRecording, stopRecording }) => {
+  return (
+    <div>
+      <button onClick={startRecording}>녹음 시작</button>
+      <button onClick={stopRecording}>녹음 중지</button>
+    </div>
+  );
 };
+
 export default Playbar;
