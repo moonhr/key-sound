a9a14e634c34e36d7d80d79a538deb45630cad36
moonhr <moonhr153@gmail.com>
2024-10-04 20:22:41 +0900

    기본키 설정

--- packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
+++ packages/key-sound/src/renderer/components/top/key_sound/key_sound.tsx
@@ -9,39 +9,44 @@ const KeySound = () => {
   // const [keySound, setKeySound] = useState<string | null>(null);
   const [selectedKeycap, setSelectedKeycap] = useState<{
     svg: string;
-  } | null>(null);
-  const { selectedSound, setSelectedSound } = useKeycap();
+    soundFile: string;
+  } | null>({
+    svg: staticData.DuckKey.svg,
+    soundFile: staticData.DuckKey.soundFile,
+  });
+  const { setSelectedSound } = useKeycap();
 
   const handleSaveKeySound = (selectedKey: string) => {
     const selectedData = staticData[selectedKey];
     setSelectedKeycap({
       svg: selectedData.svg,
+      soundFile: selectedData.soundFile,
     });
     setSelectedSound(selectedData.soundFile);
-    console.log(selectedData.soundFile);
     setModalOpen(false); // 모달 닫기
   };
 
   return (
     <div>
-      <button onClick={() => setModalOpen(true)}>모달 열기</button>
-      {/* 모달 창 */}
-      {isModalOpen && (
-        <Modal onClose={() => setModalOpen(false)}>
-          <KeycapStatic onSaveKeySound={handleSaveKeySound} />
-        </Modal>
-      )}
-
-      {/* 선택된 키캡과 사운드가 있는지 확인 */}
-      <div>
-        {selectedKeycap ? (
-          <div>
-            <img src={selectedKeycap.svg} alt="Selected Keycap" />
-          </div>
-        ) : (
-          <p>키 사운드를 선택하세요!</p>
+      <button onClick={() => setModalOpen(true)} className="w-4 h-4">
+        {/* 모달 창 */}
+        {isModalOpen && (
+          <Modal onClose={() => setModalOpen(false)}>
+            <KeycapStatic onSaveKeySound={handleSaveKeySound} />
+          </Modal>
         )}
-      </div>
+
+        {/* 선택된 키캡과 사운드가 있는지 확인 */}
+        <div>
+          {selectedKeycap ? (
+            <div>
+              <img src={selectedKeycap.svg} alt="Selected Keycap" />
+            </div>
+          ) : (
+            <p>키 사운드를 선택하세요!</p>
+          )}
+        </div>
+      </button>
     </div>
   );
 };
