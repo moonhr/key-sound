4fc69887fd6339226eefed2e169187b61472742b
moonhr <moonhr153@gmail.com>
2024-10-04 20:02:56 +0900

    파일 타입의 에러처리

--- packages/key-sound/src/renderer/components/top/play_button/play_buton.tsx
+++ packages/key-sound/src/renderer/components/top/play_button/play_buton.tsx
@@ -1,9 +1,10 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { useSoundFile } from "../../../contexts/soune_file_context";
 
 const PlayButton: React.FC = () => {
   const { soundFile } = useSoundFile();
   const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
   useEffect(() => {
     let fileUrl: string | null = null;
@@ -23,20 +24,42 @@ const PlayButton: React.FC = () => {
   }, [soundFile]);
 
   const handlePlay = () => {
+    if (soundFile?.type !== "audio/mpeg") {
+      // 파일 타입이 올바르지 않으면 경고 메시지를 설정
+      setErrorMessage("MP3 파일만 재생할 수 있습니다.");
+      return;
+    }
+
     if (audioRef.current) {
       audioRef.current.play();
+      // 재생 시 에러 메시지를 초기화
+      setErrorMessage(null);
     }
   };
 
   return (
     <div>
-      {/* 올바른 MIME 타입은 audio/mpeg */}
-      {soundFile?.type === "audio/mpeg" ? (
-        <audio ref={audioRef} controls />
-      ) : (
-        <p>MP3 파일만 재생할 수 있습니다.</p>
-      )}
-      <button onClick={handlePlay}>재생</button>
+      <button onClick={handlePlay}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="23"
+          height="29"
+          viewBox="0 0 23 29"
+          fill="none"
+        >
+          <path
+            d="M0.333252 28.7917V0.208328L22.7916 14.5L0.333252 28.7917Z"
+            fill="#1D1B20"
+          />
+        </svg>
+        {/* 에러 메시지가 있을 때만 표시 */}
+        {errorMessage && <p>{errorMessage}</p>}
+
+        {/* MP3 파일일 경우에만 audio 요소를 렌더링 */}
+        {soundFile?.type === "audio/mpeg" && (
+          <audio ref={audioRef} controls style={{ display: "none" }} />
+        )}
+      </button>
     </div>
   );
 };
