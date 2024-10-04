89d9c61f97acc8db675dc8be7a818a853d157d0a
moonhr <moonhr153@gmail.com>
2024-10-04 20:44:49 +0900

    모달창 스타일 변경과 외부 클릭시 행동 취소기능 추가

--- packages/key-sound/src/renderer/components/modal/modal.tsx
+++ packages/key-sound/src/renderer/components/modal/modal.tsx
@@ -7,9 +7,18 @@ interface ModalProps {
 }
 
 const Modal: React.FC<ModalProps> = ({ onClose, children }) => {
+  const handleOverlayClick = () => {
+    onClose();
+  };
   return (
-    <div className="modal">
-      <div className="modal-content">
+    <div
+      className="top-0 left-0 z-10 flex items-center justify-center w-full h-full bg-[#D9D9D9]  bg-opacity-70 fixed"
+      onClick={handleOverlayClick}
+    >
+      <div
+        className="p-20 bg-white border border-black w-[80%] rounded-xl"
+        onClick={(e) => e.stopPropagation()}
+      >
         <span className="close" onClick={onClose}>
           &times;
         </span>
